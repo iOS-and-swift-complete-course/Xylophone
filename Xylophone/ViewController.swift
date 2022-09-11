6c670746c290fe919adc7d9ae11bfb6d3e59a200
Chris Hand <cj.hand@gmail.com>
2022-09-10 22:38:49 -0700

    a little cleaner, and use a function to align with the lesson

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -12,14 +12,17 @@ import AVFoundation
 class ViewController: UIViewController {
 
     var player: AVAudioPlayer!
-    let soundName = ["C", "D", "E", "F", "G", "A", "B"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func keyPressed(_ sender: UIButton) {
-        let url = Bundle.main.url(forResource: soundName[sender.tag-1], withExtension: "wav")
+        playSound(key: sender.titleLabel!.text!)
+    }
+    
+    private func playSound(key: String) {
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
     }
